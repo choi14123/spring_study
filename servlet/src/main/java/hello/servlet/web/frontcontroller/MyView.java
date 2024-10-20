9181243b8a9f0c13cbe89ee716ba893fb4b41a5e
최창환 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-10-20 16:26:34 +0900

    refactor: v3, v4 기능 구현

--- servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -1,9 +1,11 @@
 package hello.servlet.web.frontcontroller;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     private String viewPath;
@@ -14,5 +16,17 @@ public class MyView {
 
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher(viewPath).forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model,
+                                         HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
