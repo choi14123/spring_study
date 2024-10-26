a9d8a0161553ed6c43475fe6400e901436c7ba30
choi14123 <ckdghks5565@gmail.com>
2024-10-26 11:52:09 +0900

    feat: thymleaf-basic study

---
+++ thymleaf-basic/src/test/java/hello/thymleaf_basic/ThymleafbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thymleaf_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymleafbasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
